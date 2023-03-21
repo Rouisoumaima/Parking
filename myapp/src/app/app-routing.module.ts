45d0b31b8e5a3735bd352f0b1c6b366cdd0aa929
m521 <mohamedismail.mezhoud@esprit.tn>
2023-03-21 16:47:02 +0100

    parkings

--- myapp/src/app/app-routing.module.ts
+++ myapp/src/app/app-routing.module.ts
@@ -1,15 +1,22 @@
+import { ParkingComponent } from './Component/parking/parking.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
+import { DashboardComponent } from './Component/dashboard/dashboard.component';
+import { UserComponent } from './Component/user/user.component';
 import { LoginComponent } from './login/login.component';
-
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
 
 
 const routes: Routes = [
   { path: '', component: LoginComponent },
-  { path: 'registration', component: RegistrationFormComponent }
- 
+  { path: 'registration', component: RegistrationFormComponent },
+  { path: 'dashboard' , component: DashboardComponent ,
+  children : [
+    {path:'user',component: UserComponent},
+    {path:'parking', component: ParkingComponent}
+  ]},
+
+
 ];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -1,28 +1,44 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { RouterModule , Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LoginComponent } from './login/login.component';
-
+import {HttpClientModule} from '@angular/common/http';
 import { RegistrationFormComponent } from './registration-form/registration-form.component';
-
+import { DashboardComponent } from './Component/dashboard/dashboard.component';
+import { UserComponent } from './Component/user/user.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ParkingComponent } from './Component/parking/parking.component';
+import { MatCardModule} from '@angular/material/card';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-  
     RegistrationFormComponent,
-  
-       
+    DashboardComponent,
+    UserComponent,
+    ParkingComponent,
+
+
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    FormsModule,
+    ReactiveFormsModule,
+    BrowserAnimationsModule,
+    HttpClientModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatButtonModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
